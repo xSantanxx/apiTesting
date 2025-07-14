41787b5b538d275abef80e6a7ed90c3200608f83
xSantanxx <dmitripower@yahoo.com>
2025-07-14 19:26:30 -0400

    added animated sprites, audio, clear button clears everything

--- script.js
+++ script.js
@@ -3,6 +3,18 @@
 function clear2(){
     const img = document.getElementById("pokemonSprite");
     img.style.visibility = "hidden";
+    const box = document.getElementById("txtBox");
+    const box2 = document.getElementById("txtBox2");
+    const typingBox = document.getElementById("pokeName");
+
+    
+    
+    box.innerHTML = "";
+    box2.innerHTML = "";
+    typingBox.value = "";
+
+    document.querySelector('button[onclick="playAudio"]').disabled = false;
+
     //alert('Hey');
 }
 
@@ -31,14 +43,16 @@ async function fetchData() {
         showDexEntry(speciesData);
 
         const pokemonSprite = data.sprites.front_default;
-        const pokemonSprite2 = data.sprites.front_shiny;
-        const dscrpItem = data.stats;
+        const pokemonAnmSpirite = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+        // const pokemonSprite2 = data.sprites.front_shiny;
+        // const dscrpItem = data.stats;
         const img = document.getElementById("pokemonSprite");
        // const img2 = document.getElementById("pokemonSprite2");
-        const dscrp = document.getElementById("dscp");
-        dscrp.src = dscrpItem;
+        // const dscrp = document.getElementById("dscp");
+        // dscrp.src = dscrpItem;
         
-        img.src = pokemonSprite;
+        // img.src = pokemonSprite;
+        img.src = pokemonAnmSpirite
         img.style.visibility = "visible";
         img.style.display = "block";
 
@@ -49,10 +63,62 @@ async function fetchData() {
 
         // console.log(data);
 
+        // document.querySelector('button[onclick="playAudio"]').disabled = false;
+
     }
     catch(error){
-        console.error(error);
+
+        const pokemonName = document.getElementById("pokeName").value.toLowerCase();
+
+        const message = `${pokemonName} , this pokemon doesn't exist in this database`
+
+        showError(message);
+        // console.error(error);
     }
+}
+
+async function playAudio(){
+
+    try {
+        const pokemonName = document.getElementById("pokeName").value.toLowerCase();
+
+        if(!pokemonName) return;
+        // don't proceed if no pokemon is entered
+
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        const data = await response.json();
+        const pokemonID = data.id;
+
+        const audio = new Audio();
+
+        audio.src = `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${pokemonID}.ogg`
+
+    audio.onerror = () => {
+        console.log("audio not found for", pokemonName);
+    }
+
+
+        audio.play().catch(error => {
+            showError("couldn't play the audio");
+        });
+    } catch(error) {
+        console.log(error);
+    }
+    
+
+
+}
+
+function showError(message){
+    const popup = document.getElementById('errorPopup');
+    popup.querySelector('p').textContent = message;
+    popup.style.display = 'block';
+}
+
+function closeError() {
+    document.getElementById('errorPopup').style.display = 'none';
 }
 
 function showStats(stats){
@@ -84,7 +150,7 @@ function showDexEntry(speciesData){
 
     //get the english flavor text
     const englishEntries = speciesData.flavor_text_entries
-        .filter(entry => entry.language.name === 'en' && entry.version.name === 'red')
+        .filter(entry => entry.language.name === 'en' && entry.version.name === 'y')
 
     if(englishEntries.length > 0){
         const firstElement = document.createElement("p");
@@ -119,10 +185,12 @@ async function backDefault() {
         
         const data = await response.json();
         const defRear = data.sprites.back_default;
+        const pokemonAnmSpirite = data.sprites.versions['generation-v']['black-white'].animated.back_default;
 
         const img = document.getElementById("pokemonSprite");
 
-        img.src = defRear;
+        // img.src = defRear;
+        img.src = pokemonAnmSpirite
 
         console.log(data);
     }
@@ -149,10 +217,12 @@ async function frontShiny() {
         
         const data = await response.json();
         const defRear = data.sprites.front_shiny;
+        const pokemonAnmSpirite = data.sprites.versions['generation-v']['black-white'].animated.front_shiny;
 
         const img = document.getElementById("pokemonSprite");
 
-        img.src = defRear;
+        // img.src = defRear;
+        img.src = pokemonAnmSpirite;
 
         console.log(data);
     }
@@ -178,10 +248,12 @@ async function backShiny() {
         
         const data = await response.json();
         const defRear = data.sprites.back_shiny;
+        const pokemonAnmSpirite = data.sprites.versions['generation-v']['black-white'].animated.back_shiny;
 
         const img = document.getElementById("pokemonSprite");
 
-        img.src = defRear;
+        // img.src = defRear;
+        img.src = pokemonAnmSpirite;
 
         console.log(data);
     }
@@ -192,11 +264,11 @@ async function backShiny() {
     
 }
 
-async function desc(){
-    try {
+// async function desc(){
+//     try {
         
-    } catch (error) {
-        console.error(error);
-    }
-}
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
 
